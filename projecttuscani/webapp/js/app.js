5403481c202456a006fc6600d15eba7cfaed787f
TremainK <tckeith7@gmail.com>
2015-10-25 17:19:10 -0400

    Added server-side dependecies, require setup

---
+++ projecttuscani/webapp/js/app.js
@@ -0,0 +1,22 @@
+define([        /* Define modules that load into the window scope */
+        'jquery',
+        'bootstrap',
+        'domReady',
+        'moment',
+        'text'
+        ],
+    function(
+        /* Since these are loaded into the window, they are defined at the window scope and it could
+         * be harmful if we define the same name. Hence the "global" prefix for the scripts that do
+         * not use requirejs.
+         */
+    	$,
+    	bootstrap,
+    	domReady,
+        moment,
+        text
+    )
+    {
+	
+    }
+);
---
+++ projecttuscani/webapp/js/constants.js
@@ -0,0 +1,15 @@
+requirejs = 
+{
+        baseUrl: 'js/',
+        //urlArgs: 'v=js_version/',
+        paths:
+        {
+        	bootstrap: 'libs/bootstrap',
+        	domReady: 'libs/domReady',
+        	jquery:'libs/jquery-2.1.4.min',
+        	moment: 'libs/moment',
+        	text: 'libs/text'
+        }
+
+};
+
