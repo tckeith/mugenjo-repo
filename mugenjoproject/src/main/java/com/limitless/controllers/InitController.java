73508e4e3ead213c05861098cee40ccb61199859
Tremain Keith <tremain.keith@fmr.com>
2016-11-14 16:11:24 -0500

    Logging

--- mugenjoproject/src/main/java/com/limitless/controllers/InitController.java
+++ mugenjoproject/src/main/java/com/limitless/controllers/InitController.java
@@ -1,10 +1,6 @@
 package com.limitless.controllers;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 @RequestMapping(value = "/test")
 public class InitController {
+	
+	private static final Logger logger = Logger.getLogger(InitController.class.getName());
 
 	@RequestMapping(value = "/run", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<?> testing(@RequestParam("data") String data) {
+		
+		logger.info("Logging param data --- >" + data);
 
 		return new ResponseEntity<String>(data, HttpStatus.OK);
 	}
